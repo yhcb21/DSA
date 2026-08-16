d67e1850cd4cccccc11cfa2b4a70386bf942e5e4
Yash <ybhondave9@gmail.com>
2026-08-16 18:47:45 +0530

    Sync Codeforces: 69A - Young Physicist [Java 21]

---
+++ CodeForces/69A_Young_Physicist.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+ 
+public class YoungPhysicist {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int x = 0;
+    int y = 0;
+    int z = 0;
+    for (int i = 0; i < n; i++) {
+      int p = sc.nextInt();
+      int q = sc.nextInt();
+      int r = sc.nextInt();
+ 
+      x = x + p;
+      y = y + q;
+      z = z + r;
+    }
+ 
+    if (x == 0 && y == 0 && z == 0) {
+      System.out.println("YES");
+    } else {
+      System.out.println("NO");
+    }
+  }
+}
